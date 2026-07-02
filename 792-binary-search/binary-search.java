68de364a2350a361a3dc4fd7313feb5521aff12d
CHOKESHWARAN <chokesh1906@gmail.com>
2026-07-02 12:00:18 +0530

    Time: 3 ms (1.55%) | Memory: 49 MB (13.91%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,12 +1,26 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int n=-1;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target)
-                n=i;
-        }
-        return n;
+        Arrays.sort(nums);
 
-        
+
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+
+            int mid = (low + high) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } 
+            else if (nums[mid] < target) {
+                low = mid + 1;
+                //return low;
+            } 
+            else {
+                high = mid - 1;
+             //return high;
+            }
+        }
+        return -1;
     }
 }
