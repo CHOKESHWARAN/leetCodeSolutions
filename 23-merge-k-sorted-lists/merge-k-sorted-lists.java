f5d68af792fe2e68a6a6fd26112d37c917c3a6fc
CHOKESHWARAN <chokesh1906@gmail.com>
2026-07-15 19:25:10 +0530

    Time: 5 ms (42.22%) | Memory: 46.7 MB (78.71%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }]\
+ ,
+ */
+import java.util.PriorityQueue;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) return null;
+        PriorityQueue<ListNode> minHeap = new PriorityQueue<>((a, b) -> a.val - b.val);
+        for (ListNode node : lists) {
+            if (node != null) {
+                minHeap.add(node);
+            }
+        }
+        
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        
+        while (!minHeap.isEmpty()) {
+            ListNode smallest = minHeap.poll();
+            curr.next = smallest;
+            curr = curr.next;
+            
+            if (smallest.next != null) {
+                minHeap.add(smallest.next);
+            }
+        }
+        
+        return dummy.next;
+    }
+}
